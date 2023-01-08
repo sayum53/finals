5c6999a8f22d491b13453488df965f2d05ee0a5a
sayum53 <khan.sayum@gmail.com>
2023-01-08 15:39:58 +0600

    Complete new7

---
+++ inside includes folder header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="test.css" type="text/css">
+
+    <title>WELCOME</title>
+    <meta charset="utf-8">
+</head>
+<body>
+
+    <header>
+        <h1>WELCOME</h1>
+    </header>
+
+    <main>
